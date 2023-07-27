4e18a5fe1f26d0fe1d4df65d677b12711bbe5522
DanaF22 <138699334+DanaF22@users.noreply.github.com>
2023-07-27 15:21:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 # import kivymd
 # import jnius as jnius
 import platform
-
+# import firebase
+from kivyauth.google_auth import initialize_google, login_google, logout_google
+import firebase_admin
+from firebase_admin import credentials
+from firebase import firebase
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.garden.matplotlib import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
+from kivymd.uix.label import MDLabel
+from kivy.uix.scrollview import ScrollView
+from kivy.app import App
+from kivy.clock import Clock
+
+
 #: import get_color_from_hex kivy.utils.get_color_from_hex
 from kivymd.uix.button import MDRoundFlatIconButton, MDFloatingActionButton, MDRectangleFlatButton, MDFlatButton
 from functools import partial
 
+from kivy.utils import platform
 import webbrowser
-# from plyer import call
+from plyer import call
 
+import bluetooth
+import asyncio
+import bleak
 
 import sqlite3
 
@@ -23,6 +39,8 @@ import sqlite3
 # from jnius import cast
 
 Window.size = (300, 500)
+
+
 
 """BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
 BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
@@ -59,7 +77,7 @@ ScreenManager:
     name: 'login'
     md_bg_color : [1,0,1,1]
     MDCard:
-        size_hint : None, None
+        size_hint : 1, 1
         size: "200dp", "300dp"
         pos_hint : {"center_x":.5, "center_y":.5}
         elevation: 3
@@ -76,6 +94,7 @@ ScreenManager:
             size_hint_y : None
             height : self.texture_size[1]
         MDTextField :
+            id: email1
             hint_text: "Email"
             # mode: "round"
             icon_right : "account"
@@ -87,6 +106,80 @@ ScreenManager:
             # line_color_focus:1,0,1,1
             # color_active: [1,1,1,1]
         MDTextField:
+            id: password1
+            hint_text: "Password"
+            icon_right: "eye-off"
+            size_hint_x: None
+            width: 220
+            pos_hint: {"center_x":.5}
+            # line_color_focus:1,0,1,1
+            # color_active:[1,1,1,1]
+            password: True
+        MDRoundFlatButton:
+            text: "LOGIN"
+            text_color: "white"
+            line_color: "white"
+            pos_hint: {"center_x":.5}
+            font_size: 15
+            on_press: app.login_callback(email1.text, password1.text)
+        MDFloatingActionButton:
+            icon: "google"
+            pos_hint: {"center_x": .5}
+            on_release: app.login()
+        MDRoundFlatButton:
+            text: "SIGN-UP"
+            text_color: "white"
+            line_color: "white"
+            pos_hint: {"center_x":.5}
+            font_size: 15
+            on_press: root.manager.current = 'signin'
+        Widget:
+            size_hint_y : None
+            height : 30
+            
+<SigninScreen>:
+    name: "signin"
+    md_bg_color : [1,0,1,1]
+    MDCard:
+        size_hint : 1, 1
+        size: "200dp", "800dp"
+        pos_hint : {"center_x":.5, "center_y":.5}
+        elevation: 3
+        md_bg_color: [99/255, 188/355, 243/355, 1]
+        # md_bg_color: [1/255, 6/255, 61/255, 1]
+        padding: 20
+        spacing: 30
+        orientation: "vertical"
+        MDLabel : 
+            text: 'SIGN-UP'
+            font_style : 'Button'
+            font_size : 45
+            halign : "center"
+            size_hint_y : None
+            height : self.texture_size[1]
+        MDTextField:
+            id: name
+            hint_text: "Name"
+            icon_right: "account"
+            size_hint_x: None
+            width: 220
+            pos_hint: {"center_x":.5}
+            # line_color_focus:1,0,1,1
+            # color_active:[1,1,1,1]
+        MDTextField :
+            id: email
+            hint_text: "Email"
+            # mode: "round"
+            icon_right : "email"
+            size_hint_x:None 
+            width : 220
+            font_size: 20
+            pos_hint : {"center_x":.5}
+            icon_left_color : [1,0,1,1]
+            # line_color_focus:1,0,1,1
+            # color_active: [1,1,1,1]
+        MDTextField:
+            id: password
             hint_text: "Password"
             icon_right: "eye-off"
             size_hint_x: None
@@ -101,13 +194,17 @@ ScreenManager:
             line_color: "white"
             pos_hint: {"center_x":.5}
             font_size: 15
+            # on_press: app.auth_email(email.text, password.text)
+            on_press: app.email_database(email.text, password.text)
             on_press: root.manager.current = 'menu'
-        Widget:
-            size_hint_y : None
-            height : 30
-            
-<SigninScreen>:
-    name: "signin"
+        MDRoundFlatButton:
+            text: "BACK"
+            text_color: "white"
+            line_color: "white"
+            pos_hint: {"center_x":.5}
+            font_size: 15
+            on_press: root.manager.current = 'login'
+    
 
 <MenuScreen>:
     name: 'menu'
@@ -142,6 +239,7 @@ ScreenManager:
             MDBottomNavigationItem:
                 name: 'screen 1'
                 icon: 'cloud'
+                on_tab_press: root.manager.get_screen('cloudscreen').bluetooth_discovery()
                 on_tab_release: root.manager.current = 'cloudscreen'
 
             MDBottomNavigationItem:
@@ -159,6 +257,55 @@ ScreenManager:
 <MainBandageScreen>:
     name: 'mainbandage'
     id: bandages
+    
+    MDLabel:
+        text: '* Bandage Info *'
+        halign: 'center'
+        size_hint_y: 1.8
+    MDLabel:
+        text: 'Current pH level: 2.3'
+        halign: 'center'
+        size_hint_y: 1.65
+        font_size: 45
+        theme_text_color: 'Custom'
+        text_color: 0,0,1,1
+    MDLabel:
+        text: 'Bandage Location: Shoulder'
+        halign:'center'
+        font_size: 25
+        size_hint_y: 1.48
+    MDLabel:
+        text: 'History of Wound (Past 7 Days)'
+        halign: 'center'
+        font_size: 25
+        size_hint_y: 1.41
+    BoxLayout:
+        orientation: 'vertical'
+        id: graph_container
+        # size_hint_y: .3
+        # height: "20"
+    MDLabel:
+        text: 'Your wound is healing!'
+        halign: 'center'
+        font_size:30
+        size_hint_y: .6
+        theme_text_color: 'Custom'
+        text_color: 0,1,0,1
+    MDCard:
+        orientation: 'vertical'
+        padding: "8dp"
+        size_hint: None, None
+        size: "200dp", "100dp"
+        pos_hint: {"center_x":0.5, "center_y": 0.15}
+        elevation: 3
+    
+        MDLabel:
+            font_size: 28
+            outline_color: 0,0,0
+            outline_width: 1
+            text: 'insert bandage specifics here'
+            size_hint_y: .4
+    
     MDIconButton:
         icon:'arrow-left'
         pos_hint: {"center_x": 0.090, "center_y": 0.95}
@@ -176,11 +323,25 @@ ScreenManager:
 <CloudScreen>:
     name: 'cloudscreen'
     id: container3
+    
     MDIconButton:
         icon:'arrow-right'
         pos_hint: {"center_x": 0.92, "center_y": 0.95}
         on_press: root.manager.current = 'menu'
 
+    ScrollView:  # Add ScrollView to wrap the container
+        size_hint_y: 0.9  # Make the ScrollView take 90% of the available height
+        pos_hint: {'top': 0.9}  # Position it below the top bar
+
+        GridLayout:
+            id: container3  # Give the GridLayout the same ID as the parent screen
+            cols: 1  # Set the number of columns to 1
+            padding: [12, 12, 12, 12]
+            spacing: [10, 10]
+            size_hint_y: None  # Disable height size_hint
+            height: self.minimum_height  # Set a fixed height to allow scrolling
+
+        
 <PhoneNumScreen1>:
     name: 'phonenumbers1'
     id: container
@@ -265,7 +426,31 @@ class MenuScreen(Screen):
     pass
 
 class CloudScreen(Screen):
-    pass
+
+    def bluetooth_discovery(self):
+        async def discover_devices():
+            devices = await bleak.discover()
+            return devices
+
+        print("Performing inquiry...")
+
+        loop = asyncio.get_event_loop()
+        nearby_devices = loop.run_until_complete(discover_devices())
+
+        print("Found {} devices".format(len(nearby_devices)))
+
+        grid_layout = self.ids.container3
+        grid_layout.clear_widgets()
+
+        for device in nearby_devices:
+            try:
+                label_text = "{} - {}".format(device.address, device.name)
+            except UnicodeEncodeError:
+                label_text = "{} - {}".format(device.address, device.name.encode("utf-8", "replace"))
+
+            button = MDRectangleFlatButton(text=label_text, size_hint=(1, 0.05))
+            grid_layout.add_widget(button)
+
 
 class BandageInfo(Screen):
     pass
@@ -277,9 +462,24 @@ class PhoneNumScreen2(Screen):
     pass
 
 class MainBandageScreen(Screen):
-    pass
+    def generate_bar_graph(self):
+        x = ["7/09/23", "7/10/23", "7/11/23", "7/12/23", "7/13/23", "7/14/23", "7/15/23"]
+        y = [0, 5, 10, 15, 20, 25, 30]
 
+        fig, ax = plt.subplots()
+        ax.bar(x, y)
 
+        # plt.ylabel("Level")
+        # plt.xlabel("Days")
+
+        plot_widget = FigureCanvasKivyAgg(fig)
+
+        self.ids.graph_container.clear_widgets()
+        self.ids.graph_container.add_widget(plot_widget)
+        graph_container = self.ids.graph_container
+        graph_container.size_hint_y = None  # Disable height size_hint
+        graph_container.height = "150dp"  # Set a fixed height (you can adjust the value as needed)
+        graph_container.pos_hint = {"center_x": 0.5, "center_y": 0.5}  # Center the container
 
 
 sm = ScreenManager()
@@ -341,21 +541,34 @@ class DemoApp(MDApp):
                             (5,'Shoulder', 0.2, 7.1)
                 ''')
     bandages.commit()"""
-                                                                
-    
-    
+
+
+
     def build(self):
         # self.theme_cls.primary_palette = 'LightBlue'
         # self.theme_cls.accent_palette = 'Blue'
+        client_id = open("client_id.txt")
+        client_secret = open("client_secret.txt")
+        initialize_google(self.after_login, self.error_listener, client_id.read(), client_secret.read())
         screen = Builder.load_string(screen_helper)
-
         return screen
+
+    def after_login(self):
+        pass
+
+    def error_listener(self):
+        pass
+
+    def login(self):
+        login_google()
 
     # adding the bandages onto screen
 
     # what happens when press one of the bandage icons
     def pressed(self, *args):
         self.root.get_screen('menu').manager.current = 'mainbandage'
+        self.root.get_screen('mainbandage').generate_bar_graph()
+
 
     # creating the bandage icons
     def on_start(self):
@@ -382,7 +595,6 @@ class DemoApp(MDApp):
             self.items.bind(on_press=self.pressed)
             self.root.get_screen('menu').ids.container1.add_widget(self.items)
         # on_press = self.root.get_screen('menu').manager.current = 'mainbandage'
-
 
     # saving phonenumbers once entered by user
     def get_data(self):
@@ -444,6 +656,7 @@ class DemoApp(MDApp):
             box = BoxLayout(size_hint=(1, None), size=(0, 70), orientation='horizontal')
             button = MDRectangleFlatButton(
                 text=str(rows[i][1]) + '\n' + str(rows[i][2]), size_hint=(0.7, 1), size = (0, 70), id=str(i + 1))
+            button.bind(on_press = lambda instance: self.make_phone_call(rows, i))
             box.add_widget(button)
             remove_button = MDRectangleFlatButton(
                 text='-', text_color='red', line_color='red', size_hint=(None, 1), size=(50, 70),
@@ -454,15 +667,76 @@ class DemoApp(MDApp):
 
         self.root.get_screen('phonenumbers1').manager.current = 'phonenumbers1'
 
-    #
-    # def make_phone_call(self, phone_number):
-    #     # formatted_phone_number = ''.join(filter(str.isdigit, phone_number))
-    #
-    #     call_url =  f"tel:{phone_number}"
-    #
-    #     try:
-    #         webbrowser.open(call_url)
-    #     except Exception as e:
-    #         print("Error:", e)
 
+    def make_phone_call(self, rows, index):
+        # formatted_phone_number = ''.join(filter(str.isdigit, phone_number))
+        phone_number = rows[index][2]
+
+
+        if platform == 'android':
+            call.makecall(phone_number)
+        else:
+            call_url =  f"tel:{phone_number}"
+
+        try:
+            webbrowser.open(call_url)
+        except Exception as e:
+            print("Error:", e)
+
+    # def auth_email(self, email, password):
+    #     config = {'apiKey': "AIzaSyAnyPC3n3JHYiTDhmfv-K8MKXA51lR1pZ4",
+    #       'authDomain': "healm-2-login.firebaseapp.com",
+    #       'projectId': "healm-2-login",
+    #       'storageBucket': "healm-2-login.appspot.com",
+    #       'messagingSenderId': "276399253320",
+    #       'appId': "1:276399253320:web:76b70d687e772cf73ab57d",
+    #       'measurementId': "G-D06175F6BW"}
+    #
+    #     # cred = credentials.Certificate('path/to/healm-2-login-firebase-adminsdk-y8yju-81ad5355d3.json')
+    #     app = firebase.initialize_app(config)
+    #     auth = app.auth()
+    #
+    #     Email = email
+    #     Password = password
+    #
+    #     user = auth.create_user_with_email_and_password(email, password)
+
+
+    def email_database(self, email, password):
+        # Initialize Firebase
+        self.firebase = firebase.FirebaseApplication('https://healm-login-default-rtdb.firebaseio.com/', None)
+
+        # Importing Data
+        data ={
+            'Email': email,
+            'Password': password
+        }
+
+        #Post Data
+        #Database Name/Table Name
+        self.firebase.post('healm-login-default-rtdb/Users', data)
+
+    def verify_login(self, email, password):
+        self.firebase = firebase.FirebaseApplication('https://healm-login-default-rtdb.firebaseio.com/', None)
+
+        #Get data
+        self.result = self.firebase.get('healm-login-default-rtdb/Users', '')
+
+        #Get Specific column like email or password
+        #Verify email and password
+        for i in self.result.keys():
+            if self.result[i]['Email'] == email:
+                if self.result[i]['Password'] == password:
+                    return True
+                    print(email+ "logged in!")
+            else:
+                return False
+
+    def login_callback(self, email, password):
+        if self.verify_login(email, password):
+            self.root.get_screen('login').manager.current = 'menu'
+        else:
+            # Handle incorrect login here
+            print("Invalid credentials")
 DemoApp().run()
+
